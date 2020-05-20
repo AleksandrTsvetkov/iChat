d348b0e2305c62c5a9517015acf27c67debac6aa
Alexander <aleksandrcw@gmail.com>
2020-05-20 14:18:12 +0300

    Remove storyboard, implement added with SwiftUI canvas preview for UIKit file.

--- iChat/ViewController.swift
+++ iChat/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
     }
-
-
 }
 
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = ViewController()
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            
+        }
+    }
+}
