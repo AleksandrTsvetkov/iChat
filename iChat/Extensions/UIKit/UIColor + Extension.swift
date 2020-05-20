87d2ef18ca5f2e48c98c6de2c10395e296a595b3
Alexander <aleksandrcw@gmail.com>
2020-05-20 16:26:00 +0300

    Add font to label extension. Change color literal to hex init in UIColor extension.

--- iChat/Extensions/UIKit/UIColor + Extension.swift
+++ iChat/Extensions/UIKit/UIColor + Extension.swift
@@ -11,15 +11,15 @@ import UIKit
 extension UIColor {
     
     static func buttonRed() -> UIColor {
-        return #colorLiteral(red: 0.8156862745, green: 0.007843137255, blue: 0.1058823529, alpha: 1)
+        return UIColor(hex: "D0021B")
     }
     
     static func mainWhite() -> UIColor {
-        return #colorLiteral(red: 0.968627451, green: 0.9725490196, blue: 0.9921568627, alpha: 1)
+        return UIColor(hex: "F7F8FD")
     }
     
     static func buttonDark() -> UIColor {
-        return #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        return UIColor(hex: "333333")
     }
     
 }
