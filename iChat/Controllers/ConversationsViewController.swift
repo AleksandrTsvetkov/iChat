aedc9319640646067eea261115e40a7147d137dc
Alexander <aleksandrcw@gmail.com>
2020-05-21 18:42:31 +0300

    Add searchBarController to both VC on TabBarController.

--- iChat/Controllers/ConversationsViewController.swift
+++ iChat/Controllers/ConversationsViewController.swift
@@ -7,12 +7,76 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ConversationsViewController: UIViewController {
+    
+    private var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCollectionView()
+    }
+    
+    private func setupSearchBar() {
+        navigationController?.navigationBar.barTintColor = .mainWhite()
+        navigationController?.navigationBar.shadowImage = UIImage()
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+    }
+    
+    private func setupCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .mainWhite()
+        view.addSubview(collectionView)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
 
+//MARK: UICollectionViewDelegate, UICollectionViewDataSource
+extension ConversationsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+//MARK: UISearchBarDelegate
+extension ConversationsViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
+    }
+}
+
+//MARK: CANVAS PREVIEW
+struct ConversationsVCProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let mainTabBarController = MainTabBarController()
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return mainTabBarController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            
+        }
     }
 }
--- iChat/Controllers/PeopleViewController.swift
+++ iChat/Controllers/PeopleViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import SwiftUI
 
 class PeopleViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSearchBar()
+    }
+    
+    private func setupSearchBar() {
+        navigationController?.navigationBar.barTintColor = .mainWhite()
+        navigationController?.navigationBar.shadowImage = UIImage()
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+    }
+}
+
+//MARK: UISearchBarDelegate
+extension PeopleViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
     }
+}
 
+//MARK: CANVAS PREVIEW
+struct PeopleVCProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let mainTabBarController = MainTabBarController()
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return mainTabBarController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            
+        }
+    }
 }
